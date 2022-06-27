88f66d347d145fb6e3e4eeb40df1b0df9ce7a30c
Christina Lu <69773627+christinalu3799@users.noreply.github.com>
2022-06-27 10:33:06 -0400

    Time: 211 ms (30.81%), Space: 47.4 MB (63.71%) - LeetHub

---
+++ 3-longest-substring-without-repeating-characters/3-longest-substring-without-repeating-characters.js
@@ -0,0 +1,39 @@
+/**
+ * @param {string} s
+ * @return {number}
+ */
+var lengthOfLongestSubstring = function(s) {
+    let max = 0
+    let windowStart = 0
+    // Hash through this object
+    // Instantiate an empty object to do this
+    let soFar = {}
+    
+    // Iterate through the length of the string (s)
+    for(let windowEnd  = 0; windowEnd < s.length; windowEnd++) {
+        // We want to grab the end letter
+        let rightChar = s[windowEnd] ;
+        
+        // If there is something already in soFar? If yes, add 1. If no, set it equal to 1
+        soFar[rightChar] = soFar[rightChar] + 1 || 1;
+        
+        // Check if soFar is greater than 1 
+        while(soFar[rightChar] > 1) {
+            // Decrement from the left 
+            let leftChar = s[windowStart];
+            
+            // What is the value of this leftChar? 
+            if(soFar[leftChar] > 1) {
+                soFar[leftChar]--;
+            } else {
+                delete soFar[leftChar];
+            }
+            
+            windowStart++;
+        }
+        
+        max = Math.max(max, (windowEnd - windowStart) + 1);
+    }
+    
+    return max
+};
