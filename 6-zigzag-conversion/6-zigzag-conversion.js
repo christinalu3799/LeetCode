4af150987e54674f296d4a6affa2e2405881f38b
Christina Lu <69773627+christinalu3799@users.noreply.github.com>
2022-07-01 13:57:56 -0400

    Time: 172 ms (25.28%), Space: 49.3 MB (27.55%) - LeetHub

---
+++ 6-zigzag-conversion/6-zigzag-conversion.js
@@ -0,0 +1,44 @@
+/**
+ * @param {string} s
+ * @param {number} numRows
+ * @return {string}
+ */
+var convert = function(s, numRows) {
+    if(numRows === 1) return s;
+    let x = 0;
+    let y = 0;
+    let goingDown = true;
+    
+    // '...' - rest/spread operator
+    // read more here: https://www.freecodecamp.org/news/javascript-rest-vs-spread-operators/#:~:text=The%20main%20difference%20between%20rest,expands%20iterables%20into%20individual%20elements.
+    
+    const rows = [...new Array(numRows)].map(_ => []);
+    
+    for(let i=0; i < s.length; i++){
+        // current character
+        const char = s[i];
+        rows[y].push(char);
+        
+        if(goingDown) {
+            
+            // check if we're at the last row 
+            if(y=== numRows -1) {
+                goingDown = false;
+                x++;
+                y--;
+            } else y++; 
+        // if NOT going down:
+        } else {
+            if(y===0) {
+                goingDown = true;
+                y++;
+            } else {
+                x++;
+                y--;
+            }
+        }
+    }    
+    
+    // convert into string with reduce function    
+    return rows.reduce((string,row) => string+row.join(''),'')
+};
