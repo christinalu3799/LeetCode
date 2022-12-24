e15e387c685917affcac26d0a05dace03538efb9
Christina Lu (She/Her) <christina.lu3799@gmail.com>
2022-12-24 18:47:47 -0500

    Time: 75 ms, Memory: 42.3 MB - LeetHub

--- best-time-to-buy-and-sell-stock-ii/best-time-to-buy-and-sell-stock-ii.js
+++ best-time-to-buy-and-sell-stock-ii/best-time-to-buy-and-sell-stock-ii.js
@@ -3,14 +3,10 @@
  * @return {number}
  */
 function maxProfit(prices) {
-  
     let profit = 0
-    
     for (let i = 0; i < prices.length - 1; i++) {
         let diff = prices[i+1] - prices[i]
-        
         diff > 0 ? profit += diff : profit
-        console.log(profit)
     }
     return profit
 };
