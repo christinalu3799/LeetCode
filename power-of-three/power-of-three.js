0965cc04c44c1298539c234fadadad0e843b0ddc
Christina Lu (She/Her) <christina.lu3799@gmail.com>
2022-12-27 11:39:02 -0500

    Time: 184 ms, Memory: 51.1 MB - LeetHub

--- power-of-three/power-of-three.js
+++ power-of-three/power-of-three.js
@@ -3,9 +3,6 @@
  * @return {boolean}
  */
 function isPowerOfThree(n) {
-    // EDGE CASE: if n = 0 
-    if (n <= 0) return false
-    
     // logb(a) = log(a) / log(b)
     return (Math.log10(n) / Math.log10(3)) % 1 == 0;
     
